89208ccfb00a506efe7bd64615f407fdce4b838c
Austin Condiff <austin.condiff@gmail.com>
2019-07-18 17:00:10 -0500

    Added content to careers and about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -16,7 +16,55 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         title="About us"
         backgroundImage="https://images.unsplash.com/photo-1556388275-bb5585725aca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"
       />
-      <Section>About us page coming soon.</Section>
+      <Section>
+        <h1>Introduction</h1>
+        <p>
+          ESP SAFETY INC. is part of the international holding company, ESP Corporation, which has been successfully operating
+          since 1973. We are a manufacturer of equipment and integrated systems for industrial safety and fire prevention. We
+          currently employ more than 250 skilled professionals globally. Our tremendous global growth is supported by numerous
+          offices and distributors worldwide. The company counts among its clientele some of the largest global oil and gas
+          producers in the world.
+        </p>
+        <p>
+          Our professional staff is dedicated to provide total customer satisfaction throughout all phases of the process,
+          including design, commissioning, as well as ongoing field support and service. The result is a total assurance that
+          our customer’s personnel and property are protected by highly reliable sensors & detectors coupled with exceptional
+          ongoing field support & customer service.
+        </p>
+        <p>
+          ESP Safety’s line of gas sensors and flame detector products have received FM Approval and are certified by the
+          American Bureau of Shipping for use in the US and Canada. In addition, the ESP Safety product line also carries
+          certifications and approvals from other global agencies further adding to our credentials of certified, robust, and
+          reliable safety devices for the industrial & hazardous environments markets.
+        </p>
+      </Section>
+      <Section>
+        <h1>Our commitment</h1>
+        <p>
+          ESP Safety is committed to an operating philosophy based on openness in communication, integrity in serving our
+          customers, fairness and concern for our employees and responsibility to the communities within which we operate.
+        </p>
+        <p>
+          Our vision is to exceed customer expectations for quality, safety, sustainability, cost, delivery and value.
+          Additionally, we are dedicated to creating a profitable business culture that is based on the following principles:
+        </p>
+      </Section>
+      <Section>
+        <h1>Our people</h1>
+        <p>Our people content coming soon.</p>
+      </Section>
+      <Section>
+        <h1>Our customers</h1>
+        <p>Our customers content coming soon.</p>
+      </Section>
+      <Section>
+        <h1>Our community</h1>
+        <p>Our community content coming soon.</p>
+      </Section>
+      <Section>
+        <h1>Our quality</h1>
+        <p>Our quality content coming soon.</p>
+      </Section>
     </React.Fragment>
   )
 }
--- src/templates/careers-page.js
+++ src/templates/careers-page.js
@@ -14,9 +14,68 @@ export const CareersPageTemplate = ({ title, content, contentComponent }) => {
     <React.Fragment>
       <Header
         title="Work for us"
-        backgroundImage="https://images.unsplash.com/photo-1542175603-f59071ce6e30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80"
+        backgroundImage="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=5102&q=80"
       />
-      <Section>Work for us page coming soon.</Section>
+      <Section>
+        <h1>Team and culture</h1>
+        <p>
+          Our team is comprised of driven, quick-witted and hardworking individuals who are not afraid to take on the most
+          challenging projects in our industry and want to create the most robust, reliable and high performing products for
+          flame and gas detection.
+        </p>
+      </Section>
+      <Section
+        backgroundImage="https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-1.2.1&auto=format&fit=crop&w=3450&q=80"
+        backgroundImageOpacity={0.33}
+        backgroundColor="#000000"
+        dark
+        parallax
+      >
+        <h1>Our Values</h1>
+        <p>IHART</p>
+        <ul>
+          <li>Ingenuity</li>
+          <li>Hard work</li>
+          <li>All in all the time</li>
+          <li>Reliability</li>
+          <li>Team Work</li>
+        </ul>
+      </Section>
+
+      <Section>
+        <h1>Our Perspective</h1>
+        <p>Quotes coming soon.</p>
+      </Section>
+
+      <Section>
+        <h1>Perks & Benefits</h1>
+        <p>
+          ESP Safety provides an excellent selection of health care insurance plans including employee and family medical,
+          dental, vision, employee and family life, short-term disability, FSA, 401 (k), employee assistance programs. You will
+          get 10 PTO days, floating holidays, paid holidays and a winter recess between Christmas and New Year’s. Each ESP team
+          member is eligible to earn additional compensation through bonuses and incentive programs which recognizes and rewards
+          outstanding performance.{' '}
+        </p>
+        <p>
+          Additional benefits include tuition reimbursement for those who want to continue their education, paid fitness
+          membership, plus a snack bar to keep you going. We work hard together and we like to have fun together too, we have
+          company outings and celebrate annual events such as Thanksgiving Potluck and White Elephant Christmas Party. Our
+          office in San Jose is conveniently located close to highways 101, 87 and 17 as well as to a light rail stop. We are
+          also within the walking distance from Japantown and downtown San Jose.
+        </p>
+      </Section>
+
+      <Section>
+        <h1>Opportunities</h1>
+        <p>
+          ESP Safety is an Equal Opportunity Employer. Applicants are considered for all positions without regard to race,
+          color, religion, sex, national origin, age, disability, sexual orientation, and ancestry, marital or veteran status.
+        </p>
+        <p>
+          We hire team members who want to make an impact which you will discover the moment you start your job. We offer you
+          room to grow and potential to help us take ESP to the next level. Join us! We need your drive and expertise.
+        </p>
+      </Section>
     </React.Fragment>
   )
 }
